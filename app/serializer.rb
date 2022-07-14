1576b37850dec2fadbc3aa58813196d10a56e24b
de-ahsan <mail.ahsan.ellahi@gmail.com>
2022-07-15 03:34:29 +0500

    Added serialize method to return data in the right format

--- app/serializer.rb
+++ app/serializer.rb
@@ -9,4 +9,11 @@ class Serializer
     @attribute = args unless args.empty?
     @attribute
   end
+
+  def serialize
+    data.date = data.date.strftime("%d-%m-%Y") unless data.instance_of?(Comment)
+    hash_values = data.to_h
+    hash_values.delete(:title) unless data.instance_of?(Post)
+    hash_values
+  end
 end
