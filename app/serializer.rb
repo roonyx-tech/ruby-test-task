1dd55f1f01d115ead4337e69cb08ed30b29de333
Alok Kumar <alokdev80@gmail.com>
2022-09-14 20:23:57 +0530

    Added required methods in Serializer to make specs green.

--- app/serializer.rb
+++ app/serializer.rb
@@ -1,2 +1,30 @@
+# frozen_string_literal: true
+
+# Serializer for required response
 class Serializer
+  class << self
+    attr_reader :attributes
+
+    def attribute(attribute, &block)
+      @attributes ||= {}
+      @attributes[attribute] = block
+    end
+  end
+
+  attr_reader :object
+
+  def initialize(object)
+    @object = object
+  end
+
+  def serialize
+    serialized_data = {}
+
+    self.class.attributes.each do |attribute, block|
+      serialized_data[attribute] = object.send(attribute)
+      serialized_data[attribute] = instance_eval(&block) unless block.nil?
+    end
+
+    serialized_data.compact
+  end
 end
