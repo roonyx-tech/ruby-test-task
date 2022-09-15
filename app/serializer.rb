2a08b1e2430a25fe39cdfb4ff0492fd56cd7ac39
samiullah <bahadursamiullah@gmail.com>
2022-09-15 10:48:03 +0430

    Create serializer method

--- app/serializer.rb
+++ app/serializer.rb
@@ -3,5 +3,43 @@ class Serializer
     attribute :title
     attribute :body
     attribute :date
+    
+  def initialize(option = {})
+  self.id = option[:id]
+
+  begin
+    self.body = option[:body]
+  rescue
+    # Ignored
+  end
+
+  begin
+    self.title = option[:title]
+  rescue
+    # Ignored
+  end
+
+  begin
+    self.date = option[:date].strftime("%d-%m-%Y")
+  rescue
+    # Ignored
+  end
+end
+
+def serialize
+    if self.class.name == 'PostSerializer'
+      {
+        id: @id,
+        title: @title,
+        date: @date,
+      }
+    else
+      {
+        id: @id,
+        body: @body
+      }
+    end
+
+  end
 
 end
