6464223ce1413f9ab1262b9a86360ef64e5e4972
Idrees Ibrahim <idrees.ibrahim@pitb.gov.pk>
2022-09-22 00:44:14 +0500

    post and comment serialize successfully run. attribute initilize in serilize class

--- app/serializer.rb
+++ app/serializer.rb
@@ -1,2 +1,18 @@
 class Serializer
+  # class variables 
+  @@attributes ||= {}
+  @@object ||= []
+  
+  class << self
+    # define attribute in post and comment file
+    def attribute(value, &block)
+      @@attributes[self] ||= {}
+      @@attributes[self][value] = block_given? ? block : nil
+    end
+    
+    ## for date time we calling strf so object is declared here
+    def object
+      @@object
+    end
+  end
 end
